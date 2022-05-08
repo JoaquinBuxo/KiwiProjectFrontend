b70c90982abbdefecaa48c4b75f1b833c7161c28
Joaquín Buxó <joaquin.buxo@gmail.com>
2022-05-08 18:37:26 +0200

    adding error handling

--- src/components/MobileContainer/MobileContainer.component.jsx
+++ src/components/MobileContainer/MobileContainer.component.jsx
@@ -3,18 +3,24 @@ import { useState } from "react";
 import MobileScreen from '../MobileScreen/MobileScreen.component';
 import MobileKeyboard from '../MobileKeyboard/MobileKeyboard.component';
 import Box from "@kiwicom/orbit-components/lib/Box";
+import Alert from "@kiwicom/orbit-components/lib/Alert";
 
 export default function MobileContainer() {
 
     const [wordCombinations, setWordCombinations] = useState([]);
     const [numPressed, setNumPressed] = useState([]);
+    const [error, setError] = useState();
     const dictionary = ['kiwi', 'ahoj', 'plane', 'train', 'hola']
 
     const getCombinations = async () => {
         const numResult = numPressed.join("");
-        const request = await fetch(`http://localhost:3001/t9/${numResult}`);
-        const response = await request.json();
-        setWordCombinations(response);
+        try{
+            const request = await fetch(`http://localhost:3001/t9/${numResult}`);
+            const response = await request.json();
+            setWordCombinations(response);
+        } catch(err) {
+            setError(err.message || 'unexpected error');
+        }
     };
 
     const handleKeyPressed = (number) => {
@@ -34,7 +40,8 @@ export default function MobileContainer() {
     return (
         <Box className="MobileContainer" borderRadius="normal" >   
             <MobileScreen handleKeyPressed={numPressed} combinations={wordCombinations}/>
-            <MobileKeyboard onNumKeyPressed={handleKeyPressed} onEnter={getCombinations} onReset={reset} onFilter={filterWords}/>           
+            { error && <Alert type="critical" icon>Something went wrong: {error}</Alert>}
+            <MobileKeyboard onNumKeyPressed={handleKeyPressed} onEnter={getCombinations} onReset={reset} onFilter={filterWords}/>          
         </Box>
     );
 }
